15b224bb9d87124f0799236ffbb9583af8b82ba0
Steve <stephenlclarke@mac.com>
2026-08-03 11:59:35 +0100

    fix: align DocC script licence header (#7)

--- scripts/make-docs.sh
+++ scripts/make-docs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# Copyright 2026 container-builder-shim project authors.
+# Copyright © 2026 Apple Inc. and the container-builder-shim project authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#   https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
